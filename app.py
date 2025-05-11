650d94787886c36d55030c0ae4554a95e1f8ede4
Diego Ampuero <diego.ampuero2@cloud.uautonoma.cl>
2025-05-11 18:36:25 -0400

    Configuración para Render corregida

--- app.py
+++ app.py
@@ -121,5 +121,6 @@ def eliminar_cliente(id):
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
+
 
