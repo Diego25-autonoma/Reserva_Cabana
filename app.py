78d05b480f2bf0025ecf0b4e79a9161d78f2072e
Diego Ampuero <diego.ampuero2@cloud.uautonoma.cl>
2025-05-11 21:31:05 -0400

    Actualización de app.py para compatibilidad

--- app.py
+++ app.py
@@ -118,9 +118,10 @@ def eliminar_cliente(id):
         return render_template('error.html', mensaje="Hubo un problema al eliminar el cliente.")
 
 # Iniciar la aplicación
+import os
+
 if __name__ == "__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
 
 
